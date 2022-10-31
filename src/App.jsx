a829245842f6f574668171c4ed4c40cfdcea5f69
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-10-31 16:19:43 -0300

    create post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './Post';
+import { Post } from './components/Post';
 
 import styles from './App.module.css';
 
@@ -10,10 +10,11 @@ export function App() {
   return (
     <div>
       <Header />
-      
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post author='Luisa' content='asdas' />
           <Post author='Luisa' content='asdas' />
         </main>
       </div>
